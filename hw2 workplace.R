5437711d092cbddabfeaec6e5851b9f719bc9a5f
yyangdev@gmail.com <yyff1225257758@gmail.com>
2018-02-18 03:09:39 -0500

    I thought link files would directly upload the original files. How foolish.

--- hw2 workplace.R
+++ hw2 workplace.R
@@ -1,1 +1,22 @@
-../hw2 workplace.R
+
+sqldf("attach 'Test1.sqlite' as new")
+
+db <- DBI::dbConnect(RSQLite::SQLite(), "hwk2Data/baseball-archive-sqlite/lahman2016.sqlite")
+
+DBI::dbGetQuery(db, 'SELECT m.playerID, sum(b.H) as total_hits, nameFirst, nameLast
+                FROM Master as m
+                  left join(SELECT distinct playerID
+                        FROM HallofFame
+                        WHERE inducted = "y") as hof
+                      on m.playerID = hof.playerID
+                  join Batting as b
+                      on m.playerID = b.playerID
+                WHERE birthCountry = "P.R."
+                    and hof.playerID is NULL
+                
+                group by m.playerID
+                order by total_hits desc
+                limit 10')
+
+DBI::dbListTables(db)
+sql, connection=db
